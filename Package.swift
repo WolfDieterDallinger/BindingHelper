d49fdaf14bbba03fc8ffa03b0ffbe8477364ed89
Wolf Dieter Dallinger <wolf.dieter@dallinger.de>
2023-02-11 02:44:50 +0100

    1.0.4: * Documentation includes extended types.

--- Package.swift
+++ Package.swift
@@ -15,10 +15,16 @@ let package = Package(
     ],
     /* To generate static documentation in a deleted docs directory uncomment swift-docc-plugin in dependencies and enter with appropriate directory in terminal:
      cd ~/Documents/Apps/RemoteSwiftPackages/BindingHelper
-     swift package --allow-writing-to-directory ~/Documents/Apps/RemoteSwiftPackages/BindingHelper/docs generate-documentation --target BindingHelper --disable-indexing --transform-for-static-hosting --hosting-base-path BindingHelper --output-path ~/Documents/Apps/RemoteSwiftPackages/BindingHelper/docs
+     swift package --allow-writing-to-directory ~/Documents/Apps/RemoteSwiftPackages/BindingHelper/docs  generate-documentation --target BindingHelper --disable-indexing --transform-for-static-hosting --hosting-base-path BindingHelper --output-path ~/Documents/Apps/RemoteSwiftPackages/BindingHelper/docs
+     
+     To generate static documentation with extended types in a deleted docs directory uncomment swift-docc-plugin in dependencies and enter with appropriate directory and appropriate swift 5.8 toolchain identifier (you will find it in the toolchains plist file) in terminal:
+      cd ~/Documents/Apps/RemoteSwiftPackages/BindingHelper
+      swift package --toolchain org.swift.58202302091a update swift-docc-plugin
+      swift package --toolchain org.swift.58202302091a --allow-writing-to-directory ~/Documents/Apps/RemoteSwiftPackages/BindingHelper/docs  generate-documentation --target BindingHelper --disable-indexing --transform-for-static-hosting --hosting-base-path BindingHelper --output-path ~/Documents/Apps/RemoteSwiftPackages/BindingHelper/docs --include-extended-types
      */
     dependencies: [
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0"),
+        .package(url: "https://github.com/apple/swift-docc-plugin", branch: "main"), // With extended types.
+        //.package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0"),
     ],
     targets: [
         .target(
