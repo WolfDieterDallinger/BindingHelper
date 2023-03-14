eb63e21ad12ae70f93b242a71d6bb3074372756c
Wolf Dieter Dallinger <wolf.dieter@dallinger.de>
2023-03-14 18:56:53 +0100

    1.0.5: * Bugfix for 1.0.4.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -18,13 +18,19 @@ let package = Package(
      swift package --allow-writing-to-directory ~/Documents/Apps/RemoteSwiftPackages/BindingHelper/docs  generate-documentation --target BindingHelper --disable-indexing --transform-for-static-hosting --hosting-base-path BindingHelper --output-path ~/Documents/Apps/RemoteSwiftPackages/BindingHelper/docs
      
      To generate static documentation with extended types in a deleted docs directory uncomment swift-docc-plugin in dependencies and enter with appropriate directory and appropriate swift 5.8 toolchain identifier (you will find it in the toolchains plist file) in terminal:
-      cd ~/Documents/Apps/RemoteSwiftPackages/BindingHelper
-      swift package --toolchain org.swift.58202302091a update swift-docc-plugin
-      swift package --toolchain org.swift.58202302091a --allow-writing-to-directory ~/Documents/Apps/RemoteSwiftPackages/BindingHelper/docs  generate-documentation --target BindingHelper --disable-indexing --transform-for-static-hosting --hosting-base-path BindingHelper --output-path ~/Documents/Apps/RemoteSwiftPackages/BindingHelper/docs --include-extended-types
+     cd ~/Documents/Apps/RemoteSwiftPackages/BindingHelper
+     swift package --toolchain org.swift.58202302231a update swift-docc-plugin
+     swift package --toolchain org.swift.58202302231a --allow-writing-to-directory ~/Documents/Apps/RemoteSwiftPackages/BindingHelper/docs generate-documentation --target BindingHelper --disable-indexing --transform-for-static-hosting --hosting-base-path BindingHelper --output-path ~/Documents/Apps/RemoteSwiftPackages/BindingHelper/docs --include-extended-types
+     
+     cd ~/Documents/Apps/RemoteSwiftPackages/BindingHelper
+     /Library/Developer/Toolchains/swift-5.8-DEVELOPMENT-SNAPSHOT-2023-02-23-a.xctoolchain/usr/bin/swift package update swift-docc-plugin
+     /Library/Developer/Toolchains/swift-5.8-DEVELOPMENT-SNAPSHOT-2023-02-23-a.xctoolchain/usr/bin/swift package --allow-writing-to-directory ~/Documents/Apps/RemoteSwiftPackages/BindingHelper/docs generate-documentation --target BindingHelper --disable-indexing --transform-for-static-hosting --hosting-base-path BindingHelper --output-path ~/Documents/Apps/RemoteSwiftPackages/BindingHelper/docs --include-extended-types
+
+     /Library/Developer/Toolchains/swift-5.8-DEVELOPMENT-SNAPSHOT-2023-02-23-a.xctoolchain/usr/bin
      */
     dependencies: [
-        .package(url: "https://github.com/apple/swift-docc-plugin", branch: "main"), // With extended types.
-        //.package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0"),
+        //.package(url: "https://github.com/apple/swift-docc-plugin", branch: "main"), // With extended types.
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.2.0"),
     ],
     targets: [
         .target(
